a75e12fe896024002f52b78ee229fbf7a65e1d67
Prakitr (Ta) Srisuma <114618495+PrakitrSrisuma@users.noreply.github.com>
2024-07-07 00:08:35 -0400

    Update CellThawing_InputPower.m

--- MATLAB & Python Code/CellThawing_InputPower.m
+++ MATLAB & Python Code/CellThawing_InputPower.m
@@ -41,7 +41,7 @@ optsim = 'off';  % simulation with the optimal heater temperature
 Ipopt = 'off';  % Ipopt
 fmincon = 'off';  % fmincon
 CasADi_ss = 'off';  % CasADi single shooting
-DAE = 'on';  % DAE-based method
+DAE = 'on';  % simulation-based method
 
 
 %% ODE solver
